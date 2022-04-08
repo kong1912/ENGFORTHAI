945911bb6a83fa6e53647a8a46bab579fa499b1b
kong1912 <69033307+kong1912@users.noreply.github.com>
2022-04-08 23:05:20 +0700

    update

--- inside/main/static/js/didn't use/score.js
+++
@@ -1,9 +0,0 @@
-function sendData() {
-    let score = document.getElementById("score").innerHTML;
-    const URL = '/pretest';
-    const xhr = new XMLHttpRequest();
-    score = JSON.stringify(score);
-    xhr.open('POST', URL);
-    xhr.send(score);
-
-}
